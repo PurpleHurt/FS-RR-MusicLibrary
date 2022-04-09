958eb162315d5dee8057731674a6f2b39a9cd8d2
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-04-09 09:41:12 -0500

    Update GalleryItem.js
    added routes

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,8 +1,27 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const GalleryItem = ({musicItem}) => {
     let [view, toggleView] = useState(false);
+
+    const simpleStyle = {
+        width: '25vw',
+        height: '20vh',
+        border: '1px solid black',
+        margin: '2px'
+    };
+    
+    const detailStyle = {
+        width: '80vw',
+        height: '20vh',
+        border: '1px solid black',
+        margin: '2px',
+        backgroundImage: `url(${musicItem.artworkUrl100})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover', 
+        color: 'white' 
+    };
 
     const simpleView = () => (
         <div style={simpleStyle}>
@@ -11,37 +30,29 @@ const GalleryItem = ({musicItem}) => {
         </div>
     );
     
-    
-
     const detailedView = () => (
         <div style={detailStyle}>
-            <h3>{musicItem.trackName}</h3>
-            <h4>{musicItem.collectionName}</h4>
+            <h2>{musicItem.trackName}</h2>
+            <h3>
+                <Link to={`{/artist/${musicItem.artistId}`}>
+                    {musicItem.artistName}
+                </Link>
+            </h3>
+            <h3>
+                <Link to={`{/album/${musicItem.collectionId}`}>
+                    {musicItem.collectionName}
+                </Link>
+            </h3>
             <h4>{musicItem.primaryGenreName}</h4>
             <h4>{musicItem.releaseDate}</h4>
         </div>
     );
 
-const simpleStyle = {
-    width: '25vw',
-    height: '20vh',
-    border: '1px solid black',
-    margin: '2px'
-}
-
-const detailStyle = {
-    width: '80vw',
-    height: '20vh',
-    border: '1px solid black',
-    margin: '2px',
-    backgroundImage: `url(${musicItem.artworkUrl100})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover', 
-    color: 'white' 
-}
-
     return (
-        <div className="gallery-item" onClick={() => toggleView(!view)}>
+        <div 
+            onClick={() => toggleView(!view)}
+            style={{ 'display': 'inline-block'}}
+            >
             {view ? detailedView() : simpleView()}
         </div>
     );
